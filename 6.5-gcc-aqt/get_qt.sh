1acee4d235b2471c5b9ffc8869ba59d7adc1517a
pandareen <sandeshyapuram@gmail.com>
2023-06-04 01:35:39 +0530

    Update get_qt.sh

--- 6.5-gcc-aqt/get_qt.sh
+++ 6.5-gcc-aqt/get_qt.sh
@@ -29,7 +29,7 @@ echo
 echo '--> Download & install the Qt library using aqt'
 echo
 
-aqt install-qt -O "$QT_PATH" linux desktop "$QT_VERSION" gcc_64
+aqt install-qt -O "$QT_PATH" linux desktop "$QT_VERSION" gcc_64 -m all
 aqt install-tool -O "$QT_PATH" linux desktop tools_cmake
 aqt install-tool -O "$QT_PATH" linux desktop tools_ninja
 
